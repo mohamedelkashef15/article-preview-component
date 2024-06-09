42e64ff2de929d5b41c220fbee896b3eabeb2a3f
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-09 15:49:58 +0300

    add static part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
 function App() {
-  return <div></div>;
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="card">
+          <DisplayImage />
+          <DisplayContent />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function DisplayImage() {
+  return <img src="images/drawers.jpg" alt="avatar-michelle" />;
+}
+
+function DisplayContent() {
+  return (
+    <div className="content">
+      <h2>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h2>
+      <p className="description">
+        Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got
+        some simple tips to help you make any room feel complete.
+      </p>
+
+      <Avatar />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="images/avatar-michelle.jpg" alt="avatar-michelle" />
+      <div>
+        <h3>Michelle Appleton</h3>
+        <p>28 Jun 2020</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
