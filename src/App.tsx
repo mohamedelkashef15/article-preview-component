dd3d251575b78d11c8e27e37cc4aaad13331afa4
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-06-10 15:15:50 +0300

    Add app logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <main className="main">
@@ -12,7 +14,7 @@ function App() {
 }
 
 function DisplayImage() {
-  return <img src="images/drawers.jpg" alt="avatar-michelle" />;
+  return <img src="images/drawers.jpg" alt="avatar-michelle" className="drawers" />;
 }
 
 function DisplayContent() {
@@ -23,8 +25,10 @@ function DisplayContent() {
         Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got
         some simple tips to help you make any room feel complete.
       </p>
-
-      <Avatar />
+      <div className="info">
+        <Avatar />
+        <Share />
+      </div>
     </div>
   );
 }
@@ -36,6 +40,28 @@ function Avatar() {
       <div>
         <h3>Michelle Appleton</h3>
         <p>28 Jun 2020</p>
+      </div>
+    </div>
+  );
+}
+
+function Share() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="share">
+      <img src="images/icon-share.svg" alt="share" onClick={() => setIsOpen(!isOpen)} />
+      <div className={`social ${isOpen ? "" : "hidden"}`}>
+        <span>SHARE</span>
+        <a href="https://facebook.com">
+          <img src="images/icon-facebook.svg" alt="facebook" />
+        </a>
+        <a href="https://pinterset.com">
+          <img src="images/icon-pinterest.svg" alt="facebook" />
+        </a>
+        <a href="https://x.com">
+          <img src="images/icon-twitter.svg" alt="facebook" />
+        </a>
       </div>
     </div>
   );
